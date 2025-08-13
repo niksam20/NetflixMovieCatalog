a3da23910390fe1a967548e68beadcdc54f7af70
Nikhil Singh <nikhilbisht0026@gmail.com>
2025-08-13 13:25:52 +0530

    workflow updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd ~/app
-python -m venv venv
+sudo apt update
+sudo apt install -y python3 python3-venv python3-pip
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python app.py
